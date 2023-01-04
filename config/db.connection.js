b15cb7f83ee6a28a11ed256eaf700714e0c84ba8
jordbort <jordan.bortner@gmail.com>
2023-01-04 18:21:47 -0500

    Updating db connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const {MONGODB_URI} = process.env
 
 mongoose.set('strictQuery', true)
-mongoose.connect(MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || MONGODB_URI)
 
 mongoose.connection.on('connected', () => console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected ... 🙌 🙌 🙌`))
 mongoose.connection.on('error', (error) => console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connection error 😥`, error))
