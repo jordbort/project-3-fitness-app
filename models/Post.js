5f46d68172f79d34f27e7cdc3bac05cce5280af1
jordbort <jordan.bortner@gmail.com>
2023-01-04 14:49:45 -0500

    Changed performance to rating in post schema

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const postSchema = new mongoose.Schema(
         image: {type: String, required: true},
         text: {type: String, required: true},
         tags: [String],
-        performance: Number,
+        rating: Number,
         difficulty: Number
     },
     { timestamps: true }
