f8993bc45cc1befc87eecd241ebd78b0dc08eeea
jordbort <jordan.bortner@gmail.com>
2023-01-07 19:30:14 -0500

    Changing Post owner to Profile instead of User

--- models/User.js
+++ models/User.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-    id: false,
-    toJSON: {
-        virtuals: true,
-        transform: (_doc, ret) => {
-          delete ret.password;
-          return ret;
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
         },
-      },
-  }
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        id: false,
+        toJSON: {
+            virtuals: true,
+            transform: (_doc, ret) => {
+                delete ret.password;
+                return ret;
+            },
+        },
+    }
 );
 
 module.exports = mongoose.model("User", userSchema);
